b77ac30a12276f6bc04a07a38aff356dcb65416f
radiium <radiium@openmailbox.org>
2017-09-08 09:40:49 +0200

    Change youtube api key

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -8,7 +8,7 @@ export class CONSTANT {
     public static YT_API_SEARCH_URL   = 'https://www.googleapis.com/youtube/v3/search';
     public static YT_API_VIDEOS_URL   = 'https://www.googleapis.com/youtube/v3/videos';
     public static YT_API_URL   = 'https://www.googleapis.com/youtube/v3/search';
-    public static YT_API_TOKEN = 'AIzaSyAJk1xUI72YYfBMgEc84gjHUX-k2AN6-B0';
+    public static YT_API_TOKEN = 'AIzaSyCUgeZ1Wous0x3Rjw3EZQQPKDQTXJB21Es';
 
     public static YT_API_AC_SEARCH_URL = 'http://suggestqueries.google.com/complete/search';
 
