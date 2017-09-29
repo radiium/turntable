aca2989008e83bb38d1e6296791e35491e4ade4b
radiium <radiium@openmailbox.org>
2017-09-30 01:14:04 +0200

    Move dragula init in edit-playlist module and clean appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,77 +1,34 @@
-import { Component, OnInit, OnDestroy, AfterContentInit } from '@angular/core';
-import { DragulaService, dragula } from 'ng2-dragula/ng2-dragula';
+import { Component, OnInit } from '@angular/core';
 import { TabsService } from './_core/services/tabs.service';
 import { Observable } from 'rxjs/Observable';
 
 import { YoutubePlayerService } from './_shared/modules/ng2-youtube-player/services/youtube-player.service';
 
-
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    viewProviders:  [DragulaService]
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy, AfterContentInit {
+export class AppComponent implements OnInit {
 
-    dragulaBagName = 'drag-drop-list';
     selectedTab: any;
-
     isLoading: Boolean = false;
 
     constructor(
-        public ytService: YoutubePlayerService,
-        private _dragulaService: DragulaService,
-        private _tabsService: TabsService) {
-            Observable.timer(3000).subscribe((res) => {
-                this.isLoading = false;
-            });
+    public ytService: YoutubePlayerService,
+    private _tabsService: TabsService) {
 
-            this._tabsService.selectedTab$
-            .subscribe((st) => {
-                this.selectedTab = st;
-            });
-            this._tabsService.setSelectedTab(1);
+        // TODO rework an 'REAL' app loader
+        Observable.timer(3000).subscribe((res) => {
+            this.isLoading = false;
+        });
 
-    }
-    ngOnInit() { this.initDragula(); }
-    ngOnDestroy() { this.destroyDragula(); }
-    ngAfterContentInit() {
-        // this.isLoading = false;
+        // Get selected tab
+        this._tabsService.selectedTab$.subscribe((st) => {
+            this.selectedTab = st;
+        });
+        this._tabsService.setSelectedTab(1);
     }
 
-    // Init dragula service options
-    initDragula() {
-        this._dragulaService.setOptions(
-            this.dragulaBagName, {
-            revertOnSpill: true,
-            moves: (el, source, handle, sibling): boolean => {
-                return el.dataset.movable === 'true';
-            },
-            copy: (el, source): boolean => {
-                return source.dataset.acceptDrop === 'false';
-            },
-            accepts: (el, target, source, sibling): boolean => {
-                return target.dataset.acceptDrop === 'true';
-            },
-        });
-        // Prevent animation on drag
-        this._dragulaService.drag.subscribe((value) => {
-            // value[1].children[0].children[0].children[0].children[1].style.marginLeft = 2;
-            // console.log('value', value[1].children[0].children[0].children[0].children[1].style.marginLeft);
-        });
-        /*
-        this.dragulaService.dragend.subscribe((el) => {
-        });
-        this.dragulaService.over.subscribe((val) => {
-        });
-        */
-    }
-
-    // Destroy dragula service
-    destroyDragula() {
-        if (!!this._dragulaService.find(this.dragulaBagName)) {
-            this._dragulaService.destroy(this.dragulaBagName);
-        }
-    }
+    ngOnInit() {}
 }
