56ab5faf0c59a433d5ad8d7eafac5a8ea64b4371
radiium <remip.dev@gmail.com>
2018-02-28 19:20:03 +0100

    Add input height on app list item component

--- src/app/_shared/components/video-list-item/video-list-item.component.ts
+++ src/app/_shared/components/video-list-item/video-list-item.component.ts
@@ -12,11 +12,21 @@ export class VideoListItemComponent implements OnInit {
     @Input() movable: boolean;
     @Input() video: Video;
     @Input() index: Video;
+    @Input() height: string;
+
+    imgHeight: string;
+    imgWidth: string;
+
+    isColumn: boolean;
 
     constructor() {
     }
 
     ngOnInit() {
+        this.height = this.height || '100px';
+        this.imgHeight = this.height;
+        this.imgWidth = (parseFloat(this.height) + (parseFloat(this.height) / 3)) + 'px';
+        this.isColumn = parseFloat(this.height) > 50;
     }
 
     handleThumUrl(thumbUrl, type) {
