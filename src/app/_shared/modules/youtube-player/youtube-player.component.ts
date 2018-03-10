8ece617a27c1b9634dbde75744fa085d0d7e1e20
radiium <remip.dev@gmail.com>
2018-03-10 20:05:39 +0100

    Add YT iframe api parameters and clean

--- src/app/_shared/modules/youtube-player/youtube-player.component.ts
+++ src/app/_shared/modules/youtube-player/youtube-player.component.ts
@@ -1,66 +1,80 @@
 import {
-  AfterContentInit,
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  EventEmitter,
-  Input,
-  OnChanges,
-  Output,
-  Renderer2,
-  SimpleChanges,
-} from '@angular/core';
+    AfterContentInit,
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    EventEmitter,
+    Input,
+    OnChanges,
+    Output,
+    Renderer2,
+    SimpleChanges,
+    } from '@angular/core';
 import { YoutubePlayerService } from './youtube-player.service';
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  selector: 'app-youtube-player',
-  template: `
-    <div id="yt-player-ngx-component"></div>
-  `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-youtube-player',
+    template: '<div id="yt-player-ngx-component"></div>'
 })
 export class YoutubePlayerComponent implements AfterContentInit {
-  @Input() videoId = '';
-  @Input() height: number;
-  @Input() width: number;
-  /**
-   * @description sets the protocol by the navigator object
-   * if there is no window, it sets a default http protocol
-   * unless the protocol is set from outside
-   */
-  @Input() protocol: string = this.getProtocol();
-  @Input() playerVars: YT.PlayerVars = {};
+    @Input() videoId = '';
+    @Input() height: number;
+    @Input() width: number;
 
-  // player created and initialized - sends instance of the player
-  @Output() ready = new EventEmitter<YT.Player>();
-  // state change: send the YT event with its state
-  @Output() change = new EventEmitter<YT.PlayerEvent>();
+    /**
+     * @description sets the protocol by the navigator object
+     * if there is no window, it sets a default http protocol
+     * unless the protocol is set from outside
+     */
+    @Input() protocol: string = this.getProtocol();
+    @Input() playerVars: YT.PlayerVars = {};
 
-  constructor(
+    // player created and initialized - sends instance of the player
+    @Output() ready = new EventEmitter<YT.Player>();
+
+    // state change: send the YT event with its state
+    @Output() change = new EventEmitter<YT.PlayerEvent>();
+
+    constructor(
     public playerService: YoutubePlayerService,
     private elementRef: ElementRef,
     private renderer: Renderer2
-  ) { }
+    ) { }
 
-  ngAfterContentInit() {
-    const htmlId = this.playerService.generateUniqueId();
-    const playerSize = { height: this.height, width: this.width };
-    const container = this.renderer.selectRootElement('#yt-player-ngx-component');
-    this.renderer.setAttribute(container, 'id', htmlId);
-    this.playerService.loadPlayerApi({
-      protocol: this.protocol
-    });
-    this.playerService.setupPlayer(htmlId, {
-      change: this.change,
-      ready: this.ready,
-    }, playerSize, this.videoId, this.playerVars);
-  }
+    ngAfterContentInit() {
+        const htmlId = this.playerService.generateUniqueId();
+        const playerSize = {
+            height: this.height,
+            width: this.width
+        };
 
-  getProtocol() {
-    const hasWindow = window && window.location;
-    const protocol = hasWindow
-      ? window.location.protocol.replace(':', '')
-      : 'http';
-    return 'https'; // protocol;
-  }
+        if (Object.keys(this.playerVars).length === 0) {
+            this.playerVars = {
+                'autoplay': 0,
+                'controls': 0,
+                'rel' : 0,
+                'fs' : 1,
+                'iv_load_policy': 3,
+                'showinfo': 0,
+                'modestbranding': 1
+            }
+        }
+        const container = this.renderer.selectRootElement('#yt-player-ngx-component');
+        this.renderer.setAttribute(container, 'id', htmlId);
+
+        this.playerService.loadPlayerApi({ protocol: this.protocol });
+        this.playerService.setupPlayer(htmlId, {
+            change: this.change,
+            ready: this.ready,
+        }, playerSize, this.videoId, this.playerVars);
+    }
+
+    getProtocol() {
+        const hasWindow = window && window.location;
+        const protocol = hasWindow
+            ? window.location.protocol.replace(':', '')
+            : 'http';
+        return 'https'; // protocol;
+    }
 }
--- src/app/_shared/modules/youtube-player/youtube-player.service.ts
+++ src/app/_shared/modules/youtube-player/youtube-player.service.ts
@@ -3,124 +3,131 @@ import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { IPlayerApiScriptOptions, IPlayerOutputs, IPlayerSize } from './models';
 
 export function win() {
-  return window;
+    return window;
 }
 
 export function YT() {
-  return win()['YT'];
+    return win()['YT'];
 }
 
 export function Player() {
-  return YT().Player;
+    return YT().Player;
 }
 
 export const defaultSizes = {
-  height: 270,
-  width: 367
+    height: 270,
+    width: 367
 };
 
 @Injectable()
 export class YoutubePlayerService {
-  api: ReplaySubject<YT.Player>;
+    api: ReplaySubject<YT.Player>;
 
-  private ytApiLoaded = false;
+    private ytApiLoaded = false;
 
-  constructor(private zone: NgZone) {
-    this.createApi();
-  }
-
-  loadPlayerApi(options: IPlayerApiScriptOptions) {
-    const doc = win().document;
-    if (!this.ytApiLoaded) {
-      this.ytApiLoaded = true;
-      const playerApiScript = doc.createElement('script');
-      playerApiScript.type = 'text/javascript';
-      playerApiScript.src = `${options.protocol}://www.youtube.com/iframe_api`;
-      doc.body.appendChild(playerApiScript);
+    constructor(private zone: NgZone) {
+        this.createApi();
     }
-  }
 
-  setupPlayer(
-    elementId: string, outputs: IPlayerOutputs, sizes: IPlayerSize,
-    videoId = '', playerVars: YT.PlayerVars) {
-    const createPlayer = () => {
-      if (Player) {
-        this.createPlayer(elementId, outputs, sizes, videoId, playerVars);
-      }
-    };
-    this.api.subscribe(createPlayer);
-  }
-
-  play(player: YT.Player) {
-    player.playVideo();
-  }
-
-  pause(player: YT.Player) {
-    player.pauseVideo();
-  }
-
-  playVideo(media: any, player: YT.Player) {
-    const id = media.id.videoId ? media.id.videoId : media.id;
-    player.loadVideoById(id);
-    this.play(player);
-  }
-
-  isPlaying(player: YT.Player) {
-    // because YT is not loaded yet 1 is used - YT.PlayerState.PLAYING
-    const isPlayerReady: any = player && player.getPlayerState;
-    const playerState = isPlayerReady ? player.getPlayerState() : {};
-    const isPlayerPlaying = isPlayerReady
-      ? playerState !== YT().PlayerState.ENDED && playerState !== YT().PlayerState.PAUSED
-      : false;
-    return isPlayerPlaying;
-  }
-
-  createPlayer(
-    elementId: string, outputs: IPlayerOutputs, sizes: IPlayerSize,
-    videoId = '', playerVars: YT.PlayerVars = {}) {
-    const playerSize = {
-      height: sizes.height || defaultSizes.height,
-      width: sizes.width || defaultSizes.width
-    };
-    const ytPlayer = Player();
-    return new ytPlayer(elementId, {
-      ...playerSize,
-      events: {
-        onReady: (ev: YT.PlayerEvent) => {
-          this.zone.run(() => outputs.ready && outputs.ready.next(ev.target));
-        },
-        onStateChange: (ev: YT.PlayerEvent) => {
-          this.zone.run(() => outputs.change && outputs.change.next(ev));
+    loadPlayerApi(options: IPlayerApiScriptOptions) {
+        const doc = win().document;
+        if (!this.ytApiLoaded) {
+            this.ytApiLoaded = true;
+            const playerApiScript = doc.createElement('script');
+            playerApiScript.type = 'text/javascript';
+            playerApiScript.src = `${options.protocol}://www.youtube.com/iframe_api`;
+            doc.body.appendChild(playerApiScript);
         }
-      },
-      playerVars,
-      videoId,
-    });
-  }
-
-  toggleFullScreen(player: YT.Player, isFullScreen: boolean | null | undefined) {
-    let { height, width } = defaultSizes;
-
-    if (!isFullScreen) {
-      height = window.innerHeight;
-      width = window.innerWidth;
     }
-    player.setSize(width, height);
-  }
 
-  // adpoted from uid
-  generateUniqueId() {
+    setupPlayer(
+    elementId: string,
+    outputs: IPlayerOutputs,
+    sizes: IPlayerSize,
+    videoId = '',
+    playerVars: YT.PlayerVars) {
+        const createPlayer = () => {
+            if (Player) {
+                this.createPlayer(elementId, outputs, sizes, videoId, playerVars);
+            }
+        };
+        this.api.subscribe(createPlayer);
+    }
+
+    play(player: YT.Player) {
+        player.playVideo();
+    }
+
+    pause(player: YT.Player) {
+        player.pauseVideo();
+    }
+
+    playVideo(media: any, player: YT.Player) {
+        const id = media.id.videoId ? media.id.videoId : media.id;
+        player.loadVideoById(id);
+        this.play(player);
+    }
+
+    isPlaying(player: YT.Player) {
+        // because YT is not loaded yet 1 is used - YT.PlayerState.PLAYING
+        const isPlayerReady: any = player && player.getPlayerState;
+        const playerState = isPlayerReady ? player.getPlayerState() : {};
+        const isPlayerPlaying = isPlayerReady
+            ? playerState !== YT().PlayerState.ENDED && playerState !== YT().PlayerState.PAUSED
+            : false;
+        return isPlayerPlaying;
+    }
+
+    createPlayer(
+    elementId: string,
+    outputs: IPlayerOutputs,
+    sizes: IPlayerSize,
+    videoId = '',
+    playerVars: YT.PlayerVars = {}) {
+
+        const playerSize = {
+            height: sizes.height || defaultSizes.height,
+            width: sizes.width || defaultSizes.width
+        };
+        const ytPlayer = Player();
+        return new ytPlayer(elementId, {
+            ...playerSize,
+            events: {
+                onReady: (ev: YT.PlayerEvent) => {
+                    this.zone.run(() => outputs.ready && outputs.ready.next(ev.target));
+                },
+                onStateChange: (ev: YT.PlayerEvent) => {
+                    this.zone.run(() => outputs.change && outputs.change.next(ev));
+                }
+            },
+            playerVars,
+            videoId,
+        });
+    }
+
+    toggleFullScreen(player: YT.Player, isFullScreen: boolean | null | undefined) {
+        let { height, width } = defaultSizes;
+
+        if (!isFullScreen) {
+            height = window.innerHeight;
+            width = window.innerWidth;
+        }
+        player.setSize(width, height);
+    }
+
+    // adpoted from uid
+    generateUniqueId() {
     const len = 7;
     return Math.random().toString(35).substr(2, len);
-  }
+    }
 
-  private createApi() {
-    this.api = new ReplaySubject(1);
-    const onYouTubeIframeAPIReady = () => {
-      if (win()) {
-        this.api.next(YT());
-      }
-    };
-    win()['onYouTubeIframeAPIReady'] = onYouTubeIframeAPIReady;
-  }
+    private createApi() {
+        this.api = new ReplaySubject(1);
+        const onYouTubeIframeAPIReady = () => {
+            if (win()) {
+                this.api.next(YT());
+            }
+        };
+        win()['onYouTubeIframeAPIReady'] = onYouTubeIframeAPIReady;
+    }
 }
