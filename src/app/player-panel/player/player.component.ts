7606a180f0b86273efab79955e14e0add7118c6e
radiium <remip.dev@gmail.com>
2018-03-11 17:19:23 +0100

    Add player loader && refactor css

--- src/app/player-panel/player/player.component.ts
+++ src/app/player-panel/player/player.component.ts
@@ -11,15 +11,12 @@ import { PlayerStateService } from 'core/services/player-state.service';
 })
 export class PlayerComponent {
 
-    prevVideoId: string;
-    height: number;
-    width: number;
-
     @Input() side: string;
-
     @Output() playerReady = new EventEmitter<any>();
     @Output() stateChange = new EventEmitter<any>();
 
+    height: number;
+    width: number;
     player: YT.Player;
     state: YT.PlayerState;
     hiddenIco: boolean;
@@ -30,12 +27,12 @@ export class PlayerComponent {
         this.height = 700;
         this.width = 1000;
         this.hiddenIco = true;
+        this.state = -1;
     }
 
     onPlayPause() {
         if (this.player) {
             this.state = this.player.getPlayerState();
-            console.log('state', this.state);
             switch (this.state) {
                 case -1:
                     break;
@@ -59,18 +56,18 @@ export class PlayerComponent {
             setTimeout(() => {
                 this.hiddenIco = true;
                 this.cdRef.detectChanges();
-                console.log('hiddenIco', this)
             }, 500);
         }
     }
 
     onPlayerReady(player) {
-        console.log('sav player ' + this.side, player)
         this.player = player;
         this.playerReady.emit(player);
     }
+
     onStateChange(event) {
         this.state = event.data;
         this.stateChange.emit(event.data);
+        this.cdRef.detectChanges();
     }
 }
