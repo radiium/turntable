13adf0db4f07809423c7c8c2ac68c6fe9b864c9f
radiium <remip.dev@gmail.com>
2018-03-11 14:31:21 +0100

    Add range slider component

---
+++ src/app/player-panel/range-slider/range-slider.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, Input, OnChanges, SimpleChanges,
+    Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+    selector: 'app-range-slider',
+    templateUrl: './range-slider.component.html',
+    styleUrls: ['./range-slider.component.scss']
+})
+export class RangeSliderComponent implements OnInit {
+
+    @Input()  name: number;
+    @Input()  min: number;
+    @Input()  max: number;
+    @Input()  step: number;
+    @Input()  value: number;
+
+    @Output() onValueChange: EventEmitter<number> = new EventEmitter<number>();
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+    }
+
+    onInputChange(event) {
+        this.changeValue(event.target.value);
+    }
+
+    increment() {
+        let value = this.value + this.step;
+        if (value >= this.max) {
+            value = this.max;
+        } else if (value <= this.min) {
+            value = this.min;
+        }
+        this.changeValue(value);
+    }
+
+    decrement() {
+        let value = this.value - this.step;
+        if (value >= this.max) {
+            value = this.max;
+        } else if (value <= this.min) {
+            value = this.min;
+        }
+        this.changeValue(value);
+    }
+
+    changeValue(value) {
+        this.value = value;
+        this.onValueChange.emit(value);
+    }
+}
