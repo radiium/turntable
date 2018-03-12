3baf9bb164d7101ef246540c79945060e2f36b38
radiium <remip.dev@gmail.com>
2018-03-12 22:28:11 +0100

    Add implement volume, play/pause and replay on player control

--- src/app/player-panel/player-control/player-control.component.ts
+++ src/app/player-panel/player-control/player-control.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges,
+import { Component, Input, OnChanges, SimpleChanges,
     ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 
 import { PlayerState } from 'core/models';
-import { PlayerStateService } from 'core/services/player-state.service';
 
 @Component({
     selector: 'app-player-control',
@@ -10,13 +9,13 @@ import { PlayerStateService } from 'core/services/player-state.service';
     styleUrls: ['./player-control.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PlayerControlComponent implements OnInit {
+export class PlayerControlComponent {
 
     @Input() side: string;
+    @Input() player: any; // YT.Player => error YT is defined;
     @Input() playerState: PlayerState;
-    player: YT.Player;
 
-    currentVolume;
+    isOnPlay: boolean;
     currentVolumeIcon: string;
     volumeIconList: Array<string> = [
         'volume_up',
@@ -25,73 +24,64 @@ export class PlayerControlComponent implements OnInit {
     ];
 
     constructor(
-    private cdRef: ChangeDetectorRef,
-    private playerStateService: PlayerStateService
-    ) {
+    private cdRef: ChangeDetectorRef) {
+        this.isOnPlay = false;
         this.currentVolumeIcon = this.volumeIconList[0]
-
-    }
-
-    ngOnInit() {
     }
 
     ngOnChanges(changes: SimpleChanges) {
-        if (!this.player && window['YT'] && changes.playerState) {
-            // this.player = window['YT'].get(this.playerState.playerId);
-            this.player = this.playerStateService.getPlayer(this.playerState.playerId);
-        }
-
         if (changes.playerState && changes.playerState.currentValue) {
-            this.currentVolume = this.playerState.volume;
-            this.updateVolumeIcon();
+            this.isOnPlay = this.playerState.state == 1;
+            this.setMute(this.playerState.volume == 0);
             this.cdRef.detectChanges();
         }
     }
 
     updateVolumeIcon() {
-        if (this.player) {
+        if (this.playerState.volume == 0) {
+            this.currentVolumeIcon = this.volumeIconList[2];
+         } else if (this.playerState.volume > 0 && this.playerState.volume < 50) {
+             this.currentVolumeIcon = this.volumeIconList[1];
 
-            const isMuted = this.player.isMuted();
-            const volume = this.player.getVolume();
-
-            if (!isMuted) {
-                this.currentVolumeIcon = this.volumeIconList[2];
-
-            } else {
-                if (volume < 1) {
-                    this.currentVolumeIcon = this.volumeIconList[2];
-                }
-                if (volume > 0 && volume < 50) {
-                    this.currentVolumeIcon = this.volumeIconList[1];
-
-                } else if (volume >= 50 && volume <= 100) {
-                    this.currentVolumeIcon = this.volumeIconList[0];
-                }
-            }
-        }
+         } else if (this.playerState.volume >= 50 && this.playerState.volume <= 100) {
+             this.currentVolumeIcon = this.volumeIconList[0];
+         }
     }
 
     setMute(mute: boolean) {
         if (this.player) {
             if (mute) {
                 this.player.mute();
+                this.currentVolumeIcon = this.volumeIconList[2];
             } else {
                 this.player.unMute();
+                this.updateVolumeIcon();
             }
-            this.updateVolumeIcon();
         }
     }
 
-    controlVolume() {
+    muteUnmute() {
         if (this.player) {
             this.setMute(!this.player.isMuted());
         }
     }
 
-
+    replay() {
+        if (this.player) {
+            this.player.seekTo(0);
+        }
+    }
 
     onPlayPause() {
-
+        if (this.player) {
+            if (this.playerState.state == 1) {
+                this.player.pauseVideo();
+                this.isOnPlay = false;
+            } else if (this.playerState.state == 2) {
+                this.player.playVideo();
+                this.isOnPlay = true;
+            }
+        }
     }
 
     hasPrev() {
