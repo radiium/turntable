112526f7a9664da4b4595c278b9d8ae1e34b0930
radiium <remip.dev@gmail.com>
2018-03-15 23:40:50 +0100

    Remove useless code

---
+++ src/app/_core/models/progress-position.ts
@@ -0,0 +1,15 @@
+export interface ProgressPosition {
+    time: number;
+    progress: number;
+}
+
+
+export interface ProgressPosition2 {
+
+    totalTime: number;
+    currentTime: number;
+
+
+    time: number;
+    progress: number;
+}
