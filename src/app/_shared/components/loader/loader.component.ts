f77fbe84c8f8d231c4d9223342bbef7e95a23e28
radiium <remip.dev@gmail.com>
2018-05-22 20:21:02 +0200

    Create custom loader component with animation && active BrowserAnimationsModule

---
+++ src/app/_shared/components/loader/loader.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss'],
+  animations: [
+      trigger('fadeInOut', [
+          transition(':enter', [
+              style({ opacity: 1 }),
+              // animate(150, style({ opacity: 1 }))
+          ]),
+          transition(':leave', [
+              animate(250, style({ opacity: 0 }))
+          ])
+      ])
+  ]
+})
+export class LoaderComponent implements OnInit {
+
+  @Input() show: boolean;
+  @Input() isGlobal: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
